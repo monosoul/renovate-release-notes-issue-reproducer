f19d400d25d33285370a6747b3266e9aa8dcfb0e
monosoul <Kloz.Klaud@gmail.com>
2022-12-22 10:27:54 +0100

    Add another plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.7.20"
     id("dev.monosoul.jooq-docker") version "3.0.0"
+    id("com.github.monosoul.markdown.page.generator") version "2.3.1.1"
 }
 
 repositories {
